f8db9d085a89f10eef523083bf5d5ca55667b8ef
Ryan Rapier <43653476+Ryeguy00@users.noreply.github.com>
2024-02-12 10:28:45 -0800

    converted from 3 space indent to 4 space indent
    self explanatory

--- Plinko.py
+++ Plinko.py
@@ -2,20 +2,20 @@
 
 # This function finds the factorial of input 'x'
 def factorial(x):
-   if x == 0:
-      return 1
-   else:
-      return x * factorial(x-1)
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x-1)
 
 # function for getting the number of rows from the user, and ensuring the input is an integer.
 def get_rows_input():
-   while True:
-      try:
-         return int(input("How many rows: "))
-      
-      except ValueError:
-         print("Your input must be a number, try again.")
-   
+    while True:
+        try:
+            return int(input("How many rows: "))
+        
+        except ValueError:
+            print("Your input must be a number, try again.")
+    
 # Start
 
 print("--==-- Plinko Odds Calculator --==--\n")
@@ -28,10 +28,10 @@ print(f"\nThere are {rows} buckets.")
 
 # List the bucket numbers
 for i in range(rows):
-   if i < rows-1:
-      print(i, end=", ")
-   else:
-      print(i)
+    if i < rows-1:
+        print(i, end=", ")
+    else:
+        print(i)
 
 print("") ## just for new line
 
@@ -43,21 +43,20 @@ print(seperator)
 
 # Populate Table with data
 for k in range(rows):
-   n = rows-1
+    n = rows-1
 
-   # calculates probability of a ball landing in bucket 'k'
-   p = factorial(n) / (factorial(k) * factorial(n - k) * (2**n))
+    # calculates probability of a ball landing in bucket 'k'
+    p = factorial(n) / (factorial(k) * factorial(n - k) * (2**n))
 
-   # convert probability to a percent with 4 decimal places
-   percent = round(p * 100, 4)
+    # convert probability to a percent with 4 decimal places
+    percent = round(p * 100, 4)
 
-   # print row
-   #print("|  " + str(k) + "\t |  " + str(percent) + "%\t|  1/" + str(round(1/p, 4)) + "\t|")
-   print(f"|  {k}\t |  {percent}%\t|  1/{round(1/p, 4)}\t|")
+    # print row
+    #print("|  " + str(k) + "\t |  " + str(percent) + "%\t|  1/" + str(round(1/p, 4)) + "\t|")
+    print(f"|  {k}\t |  {percent}%\t|  1/{round(1/p, 4)}\t|")
 
-   print(seperator)
+    print(seperator)
 
 
 # This is so the cmd window doesn't close automatically
 input("\npress Enter to close")
-
