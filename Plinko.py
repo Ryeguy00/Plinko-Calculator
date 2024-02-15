6987ccd17e52e8c6ed2aab5dfb443d41fec09675
Ryan Rapier <43653476+Ryeguy00@users.noreply.github.com>
2024-02-14 19:15:42 -0800

    Moved everything into a main() function
    also changed a few comments to be a bit cleaner.

--- Plinko.py
+++ Plinko.py
@@ -1,5 +1,6 @@
 # Plinko odds Calculator
 
+# Function Definitions
 # This function finds the factorial of input 'x'
 def factorial(x):
     if x == 0:
@@ -7,7 +8,7 @@ def factorial(x):
     else:
         return x * factorial(x-1)
 
-# function for getting the number of rows from the user, and ensuring the input is an integer.
+# This function gets the number of rows from the user, and ensures the input is an integer.
 def get_rows_input():
     while True:
         try:
@@ -18,45 +19,47 @@ def get_rows_input():
     
 # Start
 
-print("--==-- Plinko Odds Calculator --==--\n")
+def main():
 
-# Get num of rows as input
-rows = get_rows_input()
+    print("--==-- Plinko Odds Calculator --==--\n")
 
-# State the number of buckets (same as rows)
-print(f"\nThere are {rows} buckets.")
+    # Get num of rows as input
+    rows = get_rows_input()
 
-# List the bucket numbers
-for i in range(rows):
-    if i < rows-1:
-        print(i, end=", ")
-    else:
-        print(i)
+    # State the number of buckets (same as rows)
+    print(f"\nThere are {rows} buckets.")
 
-print("") ## just for new line
+    # List the bucket numbers
+    for i in range(rows):
+        if i < rows-1:
+            print(i, end=", ")
+        else:
+            print(i)
 
-# Table Headers
-seperator="-----------------------------------------"
-print(seperator)
-print("| Bucket |  Probability\t|  Fraction\t|")
-print(seperator)
+    print("") # This is just for new line
 
-# Populate Table with data
-for k in range(rows):
-    n = rows-1
-
-    # calculates probability of a ball landing in bucket 'k'
-    p = factorial(n) / (factorial(k) * factorial(n - k) * (2**n))
-
-    # convert probability to a percent with 4 decimal places
-    percent = round(p * 100, 4)
-
-    # print row
-    #print("|  " + str(k) + "\t |  " + str(percent) + "%\t|  1/" + str(round(1/p, 4)) + "\t|")
-    print(f"|  {k}\t |  {percent}%\t|  1/{round(1/p, 4)}\t|")
-
+    # Table Headers
+    seperator="-----------------------------------------"
+    print(seperator)
+    print("| Bucket |  Probability\t|  Fraction\t|")
     print(seperator)
 
+    # Populate Table with data
+    for k in range(rows):
+        n = rows-1
 
-# This is so the cmd window doesn't close automatically
-input("\npress Enter to close")
+        # calculate probability of a ball landing in bucket 'k'
+        p = factorial(n) / (factorial(k) * factorial(n - k) * (2**n))
+
+        # convert probability to a percent with 4 decimal places
+        percent = round(p * 100, 4)
+
+        # print row of data
+        print(f"|  {k}\t |  {percent}%\t|  1/{round(1/p, 4)}\t|")
+        print(seperator)
+
+    # This is so the cmd window doesn't close automatically
+    input("\npress Enter to close")
+
+if __name__ == "__main__":
+    main()
